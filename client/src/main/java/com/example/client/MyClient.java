babd35076224f430807f31d87797bf7c33fc6373
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-03-29 16:46:01 +0400

    commit 2

--- client/src/main/java/com/example/client/MyClient.java
+++ client/src/main/java/com/example/client/MyClient.java
@@ -1,7 +1,11 @@
 package com.example.client;
 
+import com.example.MyServer;
+
 public class MyClient {
     public static void main(String[] args) {
         System.out.println("Client was started");
+        MyServer myServer = new MyServer();
+        System.out.println("Server says" + myServer.sayHello());
     }
 }
--- server/src/main/java/com/example/MyServer.java
+++ server/src/main/java/com/example/MyServer.java
@@ -1,6 +1,11 @@
 package com.example;
 
 public class MyServer {
+
+    public String sayHello() {
+        return "Hello";
+    }
+
     public static void main(String[] args) {
         System.out.println("Server was started");
     }
