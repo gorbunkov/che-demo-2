f27da5b1017838564c4a530bf17bb224daac770a
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-03-29 16:26:23 +0400

    initial commit

---
+++ client/src/main/java/com/example/client/MyClient.java
@@ -0,0 +1,7 @@
+package com.example.client;
+
+public class MyClient {
+    public static void main(String[] args) {
+        System.out.println("Client was started");
+    }
+}
---
+++ server/src/main/java/com/example/MyServer.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyServer {
+    public static void main(String[] args) {
+        System.out.println("Server was started");
+    }
+}
